1521a29c7eaa872fdd210abd572de435ca031bdc
Marek Kurdej <marek@quasardb.net>
2015-12-11 11:19:18 +0100

    Test new reverse.

--- brigand/algorithms/reverse.hpp
+++ brigand/algorithms/reverse.hpp
@@ -6,97 +6,68 @@
 =================================================================================================**/
 #pragma once
 
-#include <brigand/types/integer.hpp>
-#include <brigand/sequences/size.hpp>
 #include <brigand/sequences/append.hpp>
+#include <brigand/sequences/at.hpp>
+#include <brigand/sequences/size.hpp>
+#include <brigand/types/integer.hpp>
+
+#include <utility>
 
 namespace brigand
 {
 namespace detail
 {
 
-    template <template <class...> class L, class First, class... R>
-    struct reverse_elements
-    {
-        using head = L<First>;
-        using tail = typename reverse_elements<L, R...>::type;
-        using type = typename brigand::append<tail, head>;
-    };
+template <class Seq, class Count, class Indices>
+struct reverse_elements;
 
-    template <template <class...> class L, class T0>
-    struct reverse_elements<L, T0>
-    {
-        using type = L<T0>;
-    };
+template <template <class...> class Seq, class... U, class Count, std::size_t... I>
+struct reverse_elements<Seq<U...>, Count, std::index_sequence<I...>>
+{
+    static const std::size_t last_index = Count::value - 1u;
 
-    // fast lanes
-    template <template <class...> class L, class T0, class T1>
-    struct reverse_elements<L, T0, T1>
-    {
-        using type = L<T1, T0>;
-    };
+    using list_to_reverse = Seq<U...>;
 
-    template <template <class...> class L, class T0, class T1, class T2>
-    struct reverse_elements<L, T0, T1, T2>
-    {
-        using type = L<T2, T1, T0>;
-    };
+    using type = Seq< //
+        brigand::at<  //
+            list_to_reverse,
+            brigand::size_t<last_index - I>, //
+            >...                             //
+        >;
+};
 
-    template <template <class...> class L, class T0, class T1, class T2, class T3>
-    struct reverse_elements<L, T0, T1, T2, T3>
-    {
-        using type = L<T3, T2, T1, T0>;
-    };
+//template <template <class...> class Seq, class T0>
+//struct reverse_elements<Seq, T0>
+//{
+//    using type = Seq<T0>;
+//};
 
-    template <template <class...> class L, class T0, class T1, class T2, class T3, class T4>
-    struct reverse_elements<L, T0, T1, T2, T3, T4>
-    {
-        using type = L<T4, T3, T2, T1, T0>;
-    };
+template <class Seq, class Count = brigand::size<Seq>>
+struct reverse_impl;
 
-    template <template <class...> class L, class T0, class T1, class T2, class T3, class T4, class T5>
-    struct reverse_elements<L, T0, T1, T2, T3, T4, T5>
-    {
-        using type = L<T5, T4, T3, T2, T1, T0>;
-    };
+template <template <class...> class Seq, class... U, class Count>
+struct reverse_impl<Seq<U...>, Count>
+{
+    using type = typename reverse_elements<Seq<U...>, Count, std::index_sequence_for<U...>>::type;
+};
 
-    template <template <class...> class L, class T0, class T1, class T2, class T3, class T4, class T5, class T6>
-    struct reverse_elements<L, T0, T1, T2, T3, T4, T5, T6>
-    {
-        using type = L<T6, T5, T4, T3, T2, T1, T0>;
-    };
+template <template <class...> class Seq, class... U>
+struct reverse_impl<Seq<U...>, brigand::size_t<0>>
+{
+    using type = Seq<U...>;
+};
 
-    template <template <class...> class L, class T0, class T1, class T2, class T3, class T4, class T5, class T6, class T7>
-    struct reverse_elements<L, T0, T1, T2, T3, T4, T5, T6, T7>
-    {
-        using type = L<T7, T6, T5, T4, T3, T2, T1, T0>;
-    };
-
-    template <class L, class Count = brigand::size<L>> struct reverse_impl;
-
-    template<template<class...> class L, class... U, class Count>
-    struct reverse_impl<L<U...>,Count>
-    {
-        using type = typename reverse_elements<L, U...>::type;
-    };
-
-    template<template<class...> class L, class... U>
-    struct reverse_impl<L<U...>, brigand::size_t<0>>
-    {
-        using type = L<U...>;
-    };
-
-}
+} // namespace detail
 
 namespace lazy
 {
 
-    template <typename L>
-    using reverse = typename detail::reverse_impl<L>;
+    template <typename Seq>
+    using reverse = typename detail::reverse_impl<Seq>;
 
 }
 
-    template <typename L>
-    using reverse = typename detail::reverse_impl<L>::type;
+    template <typename Seq>
+    using reverse = typename detail::reverse_impl<Seq>::type;
 
 }
